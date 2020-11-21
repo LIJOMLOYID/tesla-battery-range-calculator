98a3d2541671b08e64edb2f65a8c549bfdd3b7f7
LIJOMLOYID <lijomloyid@gmail.com>
2020-11-21 16:47:00 +0530

    Added carstats and config

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -3,9 +3,11 @@ import PropTypes from 'react';
 import './TeslaCar.css';
 
 const TeslaCar = (props) => (
+
   <div className="tesla-car">
     <div className="tesla-wheels">
       <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
+      {console.log(props)}
       <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
     </div>
   </div>
